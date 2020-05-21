7af999d4e5d15fae424bf8e42fa02d5e914b2fea
José <jjosee961310@gmail.com>
2020-05-20 22:57:06 -0500

    Comentarios

--- ASM-C++/ASM-C++/ASM-C++.cpp
+++ ASM-C++/ASM-C++/ASM-C++.cpp
@@ -1,72 +1,71 @@
 // ASM-C++.cpp : main project file.
+//Importacion de librerias necesarias
 #include "stdafx.h"
 #include "conio.h"
 #include <iostream>
 using namespace System; 
-
+//Este programa se desarrollo en Microsoft Visual C++ 2010 Express, si se desea ejecutar este proyecto muy posiblemente se necesitará descargar dicho IDE. 
+// Metodo para multiplicar
 int mul (int a, int b){
 	int valor;
-	_asm{
-		mov eax,a
-		mul b
-		mov valor, eax
-	}
-	return valor;
+	_asm{ //Palabra cable para abrir un bloque de codigo ensamblador 
+		mov eax,a // Redirecciona el valor de a a ax
+		mul b //Se realiza la multiplicación de b x a
+		mov valor, eax // El resultado se almacena en la varible "valor".
+	} // Cierre de cosigo ensamblador
+	return valor; // Valor retornado por el metodo mul.
 
 }
-
+//Metodo para restar
 int res (int a, int b){
 	int valor;
-	_asm{
+	_asm{  //Palabra cable para abrir un bloque de codigo ensamblador 
 		mov eax,a
 		sub eax,b
 		mov valor, eax
-	}
+	}  // Cierre de cosigo ensamblador
 	return valor;
 
 }
-
-
+//Metodo para dividir
 int divi (int a, int b){
 	int valor;
-	_asm{
-		mov eax,0000h// Registro extendido en ax
-	    mov edx,0000h// Registro extendido en dx
-		MOV EAX, a 
-	    MOV EBX, b      
-		DIV EBX
-		MOV valor,EAX
-	}
+	_asm{  //Palabra cable para abrir un bloque de codigo ensamblador 
+		mov eax,0000h // Registro extendido en ax
+	    mov edx,0000h // Registro extendido en dx
+		MOV EAX, a // Redirecciona el valor de "a" a AX
+	    MOV EBX, b // Redirecciona el valor de "b" a BX   
+		DIV EBX  // Se realiza la división 
+		MOV valor,EAX // El resulta se almacena en AX (cociente) y se redirecciona a la variable "valor"
+	}  // Cierre de cosigo ensamblador
 	return valor;
 
 }
-
+//Metodo para sumar 
 int suma (int a, int b){
 	int valor;
-	_asm{
+	_asm{ //Palabra cable para abrir un bloque de codigo ensamblador 
 		
-		MOV EAX,a   
-		ADD EAX,b
-		MOV valor,EAX
-	}
+		MOV EAX,a   //Redireccionamiento de "a" a AX
+		ADD EAX,b  //Se realiza la suma de a + b
+		MOV valor,EAX // Se redirecciona el resultado a la variable "valor"
+	}  // Cierre de cosigo ensamblador
 	return valor;
 
 }
-
-
-
 int main(array<System::String ^> ^args)
 {
-
    int opci;
    int val1;
    int val2;
    int resul;
-	
-
+   std::cout<< "Jose Guadalupe Magana Diaz ISC ITSUR \n";
 	do{
     std::cout<< "1-.Multiplicacion\n2-.Suma\n3-.Division\n4-.Resta\n5-.Salir\n";
 	std::cin >> opci;
+	/* Una vez que se tienen los bloques de codigo de ensamblador, simplemente se llaman los metodos
+    de forma habitual y de igual forma se le pasan los parametros de cada metodo y se recibe el resultado.
+	*/
 	switch(opci){
 		case 1:
 			std::cout<< "****Multiplicacion****\n";
@@ -78,8 +77,6 @@ int main(array<System::String ^> ^args)
 			Console:: Write("El resultado es :" + resul+"\n");
 			std::cout<< "Presione enter\n";
 	        Console:: Read();
-		   
-
 		break;
 		case 2:
 			std::cout<< "*****Suma*****\n";
@@ -88,10 +85,8 @@ int main(array<System::String ^> ^args)
 			std::cout<< "Ingresa el segundo valor\n";
 			std::cin>>val2;
 	     	resul= suma(val1,val2);
-			Console:: Write("El resultado es :" + resul+"\n");
-	    
+			Console:: Write("El resultado es :" + resul+"\n"); 
 			Console:: Read();
-			
 			break;
 		case 3:
 			std::cout<< "*****Division*****\n";
@@ -103,7 +98,6 @@ int main(array<System::String ^> ^args)
 			Console:: Write("El resultado es :" + resul+"\n");
 	        std::cout<< "Presione enter\n";
 			Console:: Read();
-			
 			break;
 		case 4:
 			std::cout<< "*****Resta****\n";
@@ -115,9 +109,7 @@ int main(array<System::String ^> ^args)
 			Console:: Write("El resultado es :" + resul+"\n");
 			std::cout<< "Presione enter\n";
 	        Console:: Read();
-		  
 			break;
-			
 	}
 	}while(opci!=5);
 	std::cout<< "Presione enter";
